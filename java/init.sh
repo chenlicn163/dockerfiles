e60e5b029fa07bfd1f0424aee3202c70bd1acc72
chenlicn163 <chenlicn163@163.com>
2018-09-20 00:37:47 +0800

    go

--- java/init.sh
+++ java/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-/var/java/apache-tomcat-8.0.52/bin/startup.sh
+nohup /var/java/kafka_2.11-2.0.0/bin/zookeeper-server-start.sh /var/java/kafka_2.11-2.0.0/config/zookeeper.properties >/dev/null 2>&1 &
+nohup /var/java/kafka_2.11-2.0.0/bin/kafka-server-start.sh /var/java/kafka_2.11-2.0.0/config/server.properties >/dev/null 2>&1 &
+/var/java/apache-tomcat-8.0.53/bin/startup.sh
 /bin/bash
